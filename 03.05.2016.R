3ce1698616a422097d3999b7036959b7ae3f4dea
Adam Hill <adamdhill@users.noreply.github.com>
2016-03-05 03:30:22 -0800

    Create 03.05.2016.R

---
+++ 03.05.2016.R
@@ -0,0 +1,13 @@
+
+library(RCurl)
+library(foreign)
+
+url <- "https://raw.githubusercontent.com/adamdhill/Blog.data/master/03.05.2016.txt"
+FB.data <- getURL(url)
+data <- read.table(textConnection(FB.data))
+
+attach(data)
+Perc <- round(interactions/sum(interactions)*100)
+Label <- paste(name, Perc)
+pie(uniques, labels = Label, main = "Percent share of total interactions on FB in February")
+
